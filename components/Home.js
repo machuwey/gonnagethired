81b716d9bb675a9f3ff9d7bb90991ed36c1cb673
Matvey <matvetron@gmail.com>
2022-11-07 11:54:35 +0100

    Let see

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,3 @@
-'use client';
 import { useState, useEffect } from "react";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const merge = require("webpack-merge");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
